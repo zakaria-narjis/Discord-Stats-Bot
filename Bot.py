1a87013a7f5efc1bc1fa498cbb45e173a86c1ec6
Zakaria Narjis <67557885+solos001@users.noreply.github.com>
2022-09-09 18:36:25 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,5 @@
 import discord as ds
 from discord.ext import tasks
-import os
 from pymongo import MongoClient
 from datetime import datetime
 from datetime import timedelta
@@ -8,7 +7,6 @@ from time import process_time_ns
 import time
 
 #Connection to MangoDB:password defined in the variables environment
-password = os.environ['password']
 password = 'ironmaiden1997'
 CONNECTION_URL = "mongodb+srv://solos:"+password+"@botsolos.muidzcw.mongodb.net/test"
 cluster = MongoClient(CONNECTION_URL)
@@ -22,7 +20,6 @@ intents.presences = True
 client = ds.Client(intents=intents)
 
 #Discord bot Token
-Token = os.environ['TOKEN']
 Token = 'OTI5NDU3MTYzOTMwMzI5MTE5.G_Iqdh.8-gONX4ZO-fDGCv-PHvA_uNmoujWmPfaKp1lkc'
 
 #Formats timedelta object to dict form {hours:hh,minutes:mm,seconds:ss}
@@ -258,7 +255,7 @@ async def on_voice_state_update(member,before,after):
 @client.event
 async def on_disconnect():
     print('DISCONNECTED...')
-
+'''
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -266,7 +263,7 @@ async def on_message(message):
     if message.content == '$$ users':
         await message.channel.send('Hello!')
     b = message.author.activities
-    c = message.author.voice
+    c = message.author.voice'''
 
 
 
